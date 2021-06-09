dd67c5aa0cca9e0fa3255325ad890aac5626fbec
Михаил Луканин <mlukanin@ics.perm.ru>
2021-06-10 03:19:11 +0500

    Обновил версию сайта

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+M.AutoInit();
+
 $('.login-button-container .btn').click(function(){
     $('.section-login').removeClass('hidden')
 });
@@ -10,12 +12,25 @@ $(".dropdown-trigger").dropdown({
     constrainWidth: false,
     coverTrigger: false
 });
+
+$('.sidenav').sidenav();
+
 $('select').formSelect();
 $('.nav-block-container .collection-item .btn').click(function(){
     let currentBtn = $(this);
     let collectionObject = currentBtn.parent();
     let collection = collectionObject.parent();
-
-
     $(collection).find('.btn').html('Занять до');
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    freeMode: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
