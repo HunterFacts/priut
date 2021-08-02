a4f95d6314c04c58b85b671a816dceedf9506782
Михаил Луканин <mlukanin@ics.perm.ru>
2021-08-03 00:53:03 +0500

    Обновил версию сайта

--- js/main.js
+++ js/main.js
@@ -13,6 +13,23 @@ $(".dropdown-trigger").dropdown({
     coverTrigger: false
 });
 
+const swiperDog = new Swiper('.swiper-container-dogs', {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination-main',
+        type: 'bullets',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-main',
+        prevEl: '.swiper-button-prev-main',
+    },
+    loop: true
+});
+
+$(".dropdown-trigger-dog").dropdown();
+
 $('.sidenav').sidenav();
 
 $('select').formSelect();
@@ -23,14 +40,20 @@ $('.nav-block-container .collection-item .btn').click(function(){
     $(collection).find('.btn').html('Занять до');
 })
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swiper-partner", {
     slidesPerView: 3,
     spaceBetween: 0,
-    freeMode: true,
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    freeMode: true, 
   });
+
+const swiperNewsMain = new Swiper('.swiper-news', {
+    speed: 400,
+    slidesPerView: 2,
+    spaceBetween: 100,
+    navigation: {
+        nextEl: '.swiper-button-next-main',
+        prevEl: '.swiper-button-prev-main',
+    },
+    loop: true
+});
 
